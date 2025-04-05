21a9ed649b9a8c9c890d725aed2342505fed1534
emperor <edetanb@gmail.com>
2025-04-05 16:57:03 +0100

    Add getter functions

--- src/Stake.sol
+++ src/Stake.sol
@@ -17,28 +17,32 @@ contract Stake {
         address stakerAddress;
         uint256 stakeTime;
     }
-    mapping (address => uint256) public addressToAmountStaked;
-    StakeData[] public stakers;
-    uint256 constant MINIMUM_STAKE_TIME = 5 minutes;
+    mapping (address => uint256) public s_addressToAmountStaked;
+    StakeData[] private s_stakers;
+    uint256 public constant MINIMUM_STAKE_TIME = 5 minutes;
+    uint256 public immutable MINIMUM_USD = 1e18;
 
     function stakeFunds() public payable{
-        require(msg.value.getConversionRate(s_priceFeed) >= 1e18, "You must stake above $1 worth of ETH");
-        addressToAmountStaked[msg.sender] += msg.value;
+        require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "You must stake above $1 worth of ETH");
+        s_addressToAmountStaked[msg.sender] += msg.value;
         StakeData memory newStakeData = StakeData({
             amountStaked: msg.value,
             stakerAddress: msg.sender,
             stakeTime: block.timestamp
 
         });
-        stakers.push(newStakeData);
+        s_stakers.push(newStakeData);
+    }
+    function getVersion() public view returns (uint256) {
+        return s_priceFeed.version();
     }
     
     function withdraw() public {
-        require(addressToAmountStaked[msg.sender] > 0, "You have no stake");
-        uint256 stakersLength = stakers.length;
+        require(s_addressToAmountStaked[msg.sender] > 0, "You have no stake");
+        uint256 stakersLength = s_stakers.length;
         for (uint256 i = 0; i < stakersLength; i++){
-            if (stakers[i].stakerAddress == msg.sender) {
-                StakeData memory userStake = stakers[i];
+            if (s_stakers[i].stakerAddress == msg.sender) {
+                StakeData memory userStake = s_stakers[i];
 
                 require(block.timestamp >= userStake.stakeTime + MINIMUM_STAKE_TIME,
                 "Staking time not yet passed"
@@ -46,10 +50,10 @@ contract Stake {
 
                 uint256 totalAmount = userStake.amountStaked;
 
-                addressToAmountStaked[msg.sender] =0;
+                s_addressToAmountStaked[msg.sender] =0;
 
-                userStake = stakers[stakersLength - 1];
-                stakers.pop();
+                userStake = s_stakers[stakersLength - 1];
+                s_stakers.pop();
 
                 (bool callSuccess,) = payable(msg.sender).call{value: totalAmount}("");
                  require(callSuccess, "Call failed");
@@ -57,6 +61,13 @@ contract Stake {
             }
         }
     }
-        
+    function getStakers(uint256 index) external view returns(StakeData memory) {
+            return s_stakers[index];
+    }  
+    function getAddressToAmountStaked(address stakerAddress) external view returns(uint256){
+        return s_addressToAmountStaked[stakerAddress];
+    }
+
+   
    
 }
